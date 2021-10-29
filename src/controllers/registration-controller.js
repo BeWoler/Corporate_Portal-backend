df4860c3ad597c77976c41c5699cbd07765c7086
BeWoler <bewoler.del@gmail.com>
2021-10-29 17:49:50 +0300

    refactoring

--- src/controllers/registration-controller.js
+++ src/controllers/registration-controller.js
@@ -1,10 +1,15 @@
-const registrationService = require("../services/registration-service.js");
+const registrationService = require("../services/registration-service");
+const { validationResult } = require("express-validator");
 
 class RegistrationController {
   async registration(req, res) {
     try {
       const { email, password, username } = req.body;
-      const userData = await registrationService.registration(email, password, username);
+      const userData = await registrationService.registration(
+        email,
+        password,
+        username
+      );
       return res.json(userData);
     } catch (e) {
       console.log(e);
