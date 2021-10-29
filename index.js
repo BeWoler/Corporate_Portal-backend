90bec5b15faeb5e7d60daae8b99caf52ba90f97a
BeWoler <bewoler.del@gmail.com>
2021-10-29 15:16:03 +0300

    connection to DB

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-require('dotenv').config;
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-        methods: ["GET", "POST"]
-    })
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST"],
+  })
 );
 
-const connection = () => app.listen(process.env.PORT, () => console.log('Server is running'));
+const connection = async () => {
+  await mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT} port`);
+  });
+};
 
 connection();
