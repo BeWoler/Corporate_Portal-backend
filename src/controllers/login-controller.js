f0bfdbb5f898df1de287053c7b3ed045fae0304f
BeWoler <bewoler.del@gmail.com>
2021-10-29 16:48:30 +0300

    creating login controller

---
+++ src/controllers/login-controller.js
@@ -0,0 +1,15 @@
+const loginService = require("../services/login-service");
+
+class LoginController {
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+      const userData = await loginService.login(username, password);
+      return res.json(userData);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.export = new LoginController();
