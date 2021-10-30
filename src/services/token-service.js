176d2b088362a5a9cc9354f73c9629817bc2316b
BeWoler <bewoler.del@gmail.com>
2021-10-30 20:47:04 +0300

    creating token generation

---
+++ src/services/token-service.js
@@ -0,0 +1,27 @@
+const tokenModel = require("../models/token-model");
+const jwt = require("jsonwebtoken");
+
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: "30m",
+    });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: "2d",
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await tokenModel.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+    const token = await tokenModel.create({ user: userId, refreshToken });
+    return token;
+  }
+}
