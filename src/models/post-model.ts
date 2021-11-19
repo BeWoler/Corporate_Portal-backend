334ff87d745950151ad47162a60327b029120cc5
BeWoler <bewoler.del@gmail.com>
2021-11-19 23:20:59 +0300

    added new field in the post model

--- src/models/post-model.ts
+++ src/models/post-model.ts
@@ -1,14 +1,18 @@
-import { Schema, model } from 'mongoose';
-import mongoose from 'mongoose';
+import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
 interface Post {
   user: mongoose.ObjectId;
   post: object;
+  likes: number;
+  comments: string;
 }
 
-const schema = new Schema<Post> ({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  post: { type: Object }
-})
+const schema = new Schema<Post>({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  post: { type: Object },
+  likes: { type: Number },
+  comments: { type: String },
+});
 
 export const PostModel = model<Post>("Post", schema);
