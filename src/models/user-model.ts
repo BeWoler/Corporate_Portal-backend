5f71205479dc3772bb28f513f55f6f4f86ca322d
BeWoler <bewoler.del@gmail.com>
2021-11-19 16:30:03 +0300

    changed edit service

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,23 +1,41 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 interface User {
-  email: string,
-  password: string,
-  username: string,
-  age: number,
-  firstName: string,
-  lastName: string,
-  description: string
+  email: string;
+  password: string;
+  username: string;
+  userInfo: object;
+  birthday: number;
+  firstName: string;
+  lastName: string;
+  stack: string;
+  position: string;
+  department: string;
+  education: string;
+  skype: string;
+  phone: number;
+  description: string;
 }
 
 const schema = new Schema<User>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   username: { type: String, required: true, unique: true },
-  age: { type: Number, default: 0},
-  firstName: { type: String, default: ''},
-  lastName: { type: String, default: ''},
-  description: { type: String, default: ''},
-})
+  userInfo: {
+    type: Object,
+    default: {
+      birthday: { type: Date, trim: true, default: 0 },
+      firstName: { type: String, default: "" },
+      lastName: { type: String, default: "" },
+      description: { type: String, default: "" },
+      stack: { type: String, default: "" },
+      position: { type: String, default: "" },
+      department: { type: String, default: "" },
+      education: { type: String, default: "" },
+      skype: { type: String, default: "" },
+      phone: { type: Number, default: 0 },
+    },
+  },
+});
 
-export const UserModel = model<User>('User', schema);
+export const UserModel = model<User>("User", schema);
--- src/services/editUser-service.ts
+++ src/services/editUser-service.ts
@@ -3,8 +3,8 @@ import { TokenService } from "./token-service";
 import { UserDto } from "../dtos/user-dto";
 
 export class EditUserService {
-  public static async editUser(username: string, {...args}: Object) {
-    const user = await UserModel.findOneAndUpdate({username}, {...args})
+  public static async editUser(username: string, userInfo: Object) {
+    const user = await UserModel.findOneAndUpdate({username}, userInfo)
 
     const userDto = new UserDto(user);
 
