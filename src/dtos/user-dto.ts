be61ee441e4155f54dd8881dc6048e609f5418bf
BeWoler <bewoler.del@gmail.com>
2021-11-21 01:11:17 +0300

    refactoring

--- src/dtos/user-dto.ts
+++ src/dtos/user-dto.ts
@@ -3,11 +3,33 @@ import { ObjectId } from "mongoose";
 export class UserDto {
   email: string;
   username: string;
+  city: string;
+  birthday: Date;
+  firstName: string;
+  lastName: string;
+  stack: string;
+  position: string;
+  department: string;
+  education: string;
+  skype: string;
+  phone: number;
+  description: string;
   id: ObjectId;
 
   constructor(model) {
     this.email = model.email;
     this.username = model.username;
     this.id = model._id;
+    this.city = model.city;
+    this.birthday = model.birthday;
+    this.firstName = model.firstName;
+    this.lastName = model.lastName;
+    this.stack = model.stack;
+    this.position = model.position;
+    this.department = model.department;
+    this.education = model.education;
+    this.skype = model.skype;
+    this.phone = model.phone;
+    this.description = model.description;
   }
 }
--- src/services/editUser-service.ts
+++ src/services/editUser-service.ts
@@ -1,21 +1,18 @@
 import { UserModel } from "../models/user-model";
 import { TokenService } from "./token-service";
 import { UserDto } from "../dtos/user-dto";
-import { UserInfoDto } from "../dtos/userInfo-dto";
 
 export class EditUserService {
   public static async editUser(username: string, { ...args }: Object) {
     const user = await UserModel.findOneAndUpdate({ username }, { ...args });
 
     const userDto = new UserDto(user);
-    const userInfoDto = new UserInfoDto(user);
 
     const tokens = TokenService.generateTokens({ ...userDto });
     await TokenService.saveToken(userDto.id, tokens.refreshToken);
 
     return {
       user: userDto,
-      userInfo: userInfoDto,
       ...tokens,
     };
   }
