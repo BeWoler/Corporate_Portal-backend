05102a5431534b62f3b0991ffcc871d1f69e73f7
BeWoler <bewoler.del@gmail.com>
2021-11-22 15:09:55 +0300

    described post model

--- src/models/post-model.ts
+++ src/models/post-model.ts
@@ -5,14 +5,18 @@ interface Post {
   user: mongoose.ObjectId;
   post: object;
   likes: number;
-  comments: string;
+  comments: object;
 }
 
 const schema = new Schema<Post>({
   user: { type: Schema.Types.ObjectId, ref: "User" },
-  post: { type: Object },
-  likes: { type: Number },
-  comments: { type: String },
+  post: { type: Object, required: true },
+  likes: { type: Number, default: 0 },
+  comments: {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    text: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+  },
 });
 
 export const PostModel = model<Post>("Post", schema);
