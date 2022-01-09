77c817f942baba6e1098f2bc089333e0c6e7d6de
BeWoler <bewoler.del@gmail.com>
2022-01-09 20:35:59 +0300

    refactoring

--- src/services/delete-service.ts
+++ src/services/delete-service.ts
@@ -1,19 +1,23 @@
 import { PostModel } from "../models/post-model";
 import { UserModel } from "../models/user-model";
 import { TokenService } from "./token-service";
-import mongoose from "mongoose";
 import { LikeModel } from "../models/like-model";
 import { CommentModel } from "../models/comment-model";
 import { ConversationModel } from "../models/conversation-model";
+import { UserPasswordModel } from "../models/userpassword-model";
+import mongoose from "mongoose";
 
 export class DeleteService {
   public static async delete(refreshToken: string, userId: mongoose.ObjectId) {
     const token = await TokenService.removeToken(refreshToken);
     const user = await UserModel.deleteOne({ _id: userId });
+    const userPassword = await UserPasswordModel.deleteOne({ user: userId });
     const posts = await PostModel.deleteMany({ user: userId });
     const comments = await CommentModel.deleteMany({ user: userId });
     const likes = await LikeModel.deleteMany({ user: userId });
-    const conversations = await ConversationModel.deleteMany({ members: userId });
-    return { user, posts, comments, likes, conversations, token };
+    const conversations = await ConversationModel.deleteMany({
+      members: userId,
+    });
+    return { user, userPassword, posts, comments, likes, conversations, token };
   }
 }
