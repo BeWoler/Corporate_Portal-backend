3de52ff9a99cf390e10dd475a77024aa8897e39b
BeWoler <bewoler.del@gmail.com>
2022-01-30 17:43:05 +0300

    refactoring

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.use(
 app.use(cookieParser());
 app.use("/api", router);
 app.use("/admin", adminRouter);
-app.use("/files", express.static("public"));
+app.use("/files", express.static("files"));
 app.use(errorMiddleware);
 
 export const server = require("http").Server(app);
--- src/middlewares/multerAvatar-middleware.ts
+++ src/middlewares/multerAvatar-middleware.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "././public");
+    cb(null, "././files");
   },
   filename(req, file, cb) {
     if (
